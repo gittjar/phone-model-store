c7fede6be657530c1873b033b6fc31b5d60416bb
gittjar <jarno.keranen@gmail.com>
2023-03-04 20:52:31 +0200

    alerts and fixes

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,6 +4,8 @@ export interface Product {
   price: number;
   description: string;
   image: string | any;
+  varastossa: boolean;
+  mainmaterial: string;
 }
 
 export const products = [
@@ -12,7 +14,10 @@ export const products = [
     name: 'Samsung Flip 7',
     price: 799,
     description: '"Flippiseiska" suurella näytöllä, 5G nettinopeus ja tehokas akku. ',
-    image: '/assets/kuvat/pieni-samsung.png'
+    image: '/assets/kuvat/pieni-samsung.png',
+    mainmaterial: "titanium",
+    os: "Android 4x",
+    varastossa: true
 
   },
   {
@@ -20,16 +25,20 @@ export const products = [
     name: 'iPhone X',
     price: 999,
     description: 'iPhone X huippupuhelin. Omenaperheen uusin kärkituote.',
-    image: '/assets/kuvat/pieni-iphone-x.png'
+    image: '/assets/kuvat/pieni-iphone-x.png',
+    mainmaterial: "aluminium",
+    os: "iOS 15x",
+    varastossa: true
   },
   {
     id: 3,
     name: 'iPhone 7',
     price: 799,
     description: 'Pieni iPhone 7 luotettava peruspuhelin, jossa toimii kaikki.',
-    image: '/assets/kuvat/pieni-iphone-7.png'
-
-  },
-  
+    image: '/assets/kuvat/pieni-iphone-7.png',
+    mainmaterial: "aluminium",
+    os: "iOS 15x",
+    varastossa: false
+  }
 ];
 
